898926c6e22b4ae5f3af56eeea8133950b720b4e
andrewangelle <andrewangelle@gmail.com>
2016-12-15 02:37:02 -0600

    added header and navigation id to html

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 window.addEventListener('scroll', function(e){
-	var scrollTop = (window.pageYOffset !==100px) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-	var imageHeight = document.getElementById('#header-text').clientHeight
-	var navigation = document.getElementsByClassName('.navigation');
+	var scrollTop = window.pageYOffset;
+	var imageHeight = document.getElementById('header').clientHeight;
+	var navigation = document.getElementbyId('navigation');
+
 	if(scrollTop > imageHeight){
 		navigation.classList.add('fixed-nav')
 	}
 	else{
 		navigation.classList.remove('fixed-nav');
 	}
-}
 });
