fb56606fa9f6dcf851c2e2472176f5d1e28051b4
andrewangelle <andrewangelle@gmail.com>
2016-12-15 20:20:59 -0600

    attempted to clone HTML element 'headertext' and reinsert it with a fixed position once user has scrolled passed the original header element

--- main.js
+++ main.js
@@ -3,10 +3,23 @@ window.addEventListener('scroll', function(e){
 	var imageHeight = document.getElementById('header').clientHeight;
 	var navigation = document.getElementById('navigation');
 
+	var headerText = document.getElementById('headertext');
+	var clone = element.cloneNode(true);
+	var removeClone = element.cloneNode(false);
 	if(scrollTop > imageHeight){
-		navigation.classList.add('fixed-nav')
+		navigation.classList.add('fixed-nav');
+		document.getElementById('headertext').appendChild(clone);
+		
+//Attempting to re-insert the cloned 'header-text' element and add 'position:fixed'  once user has scrolled past the original header element
+
+		/*fixedLogo.cloneNode(true);
+		headerText.classList.add('fixed-logo');*/
 	}
+
 	else{
 		navigation.classList.remove('fixed-nav');
+		document.getElementById('headertext').appendChild(removeClone);
+		/*fixedLogo.cloneNode(false);
+		headerText.classList.remove('fixed-logo');*/
 	}
 });
