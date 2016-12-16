33876f8215654f1f3b707d49f3dbc1f16d3cb614
andrewangelle <andrewangelle@gmail.com>
2016-12-15 20:38:39 -0600

    removed first attempt JS code to clone html element and added second attempt JS code

--- main.js
+++ main.js
@@ -3,23 +3,23 @@ window.addEventListener('scroll', function(e){
 	var imageHeight = document.getElementById('header').clientHeight;
 	var navigation = document.getElementById('navigation');
 
-	var headerText = document.getElementById('headertext');
-	var clone = element.cloneNode(true);
-	var removeClone = element.cloneNode(false);
 	if(scrollTop > imageHeight){
 		navigation.classList.add('fixed-nav');
-		document.getElementById('headertext').appendChild(clone);
-		
-//Attempting to re-insert the cloned 'header-text' element and add 'position:fixed'  once user has scrolled past the original header element
 
-		/*fixedLogo.cloneNode(true);
-		headerText.classList.add('fixed-logo');*/
+//Attempting to clone HTML element 'header-text', insert it, and add 'position:fixed'to its CSS  once user has scrolled past the original header element
+		function cloneLogo() {
+    var headerText = document.getElementsByTagName('headertext')[0];
+    var cln = elmnt.cloneNode(true);
+    document.body.appendChild(cln);}
+		
+		headerText.classList.add('fixed-logo');
 	}
 
 	else{
 		navigation.classList.remove('fixed-nav');
+
+//Attempting to remove the cloned 'header-text' element and its CSS if user has not scrolled past original header element.
 		document.getElementById('headertext').appendChild(removeClone);
-		/*fixedLogo.cloneNode(false);
-		headerText.classList.remove('fixed-logo');*/
+		headerText.classList.remove('fixed-logo');
 	}
 });
