fc1cbac9e366b3180a210a440a3c79933a1befce
andrewangelle <andrewangelle@gmail.com>
2016-12-20 20:10:57 -0600

    fixed scrolling issues between the content and body containers

--- main.js
+++ main.js
@@ -26,12 +26,12 @@ function slideHeaderImages() {
 	}
 ;}
 */
-
 window.addEventListener('scroll', function(e){
 	var scrollTop = window.pageYOffset;
 	var imageHeight = document.getElementById('header').clientHeight;
 	var navigation = document.getElementById('navigation');
 	var headerText = document.getElementById('header-text');
+
 
 	if(scrollTop > imageHeight){
 		navigation.classList.add('fixed-nav');
