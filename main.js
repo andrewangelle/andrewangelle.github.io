ae9bf78d4d99f43c12fab605fd9b356905d01bcd
andrewangelle <andrewangelle@gmail.com>
2017-01-04 15:13:22 -0600

    responsive nav changes

--- main.js
+++ main.js
@@ -1,29 +1,44 @@
-
-window.addEventListener('scroll', function(e){
-	var scrollTop = window.pageYOffset;
-	var imageHeight = document.getElementById('header').clientHeight;
-	var navigation = document.getElementById('navigation');
-	var headerText = document.getElementById('header-text');
-
-
-	if(scrollTop > imageHeight){
-		navigation.classList.add('fixed-nav');
-		headerText.classList.add('fixed-logo');
-	}
-	else{
-		navigation.classList.remove('fixed-nav');
-		headerText.classList.remove('fixed-logo');
-	}
-});
-
-document.getElementById('nav-icon').addEventListener('click',function(){
-	var navLinks = document.getElementById('nav-links');
-
-	navLinks.classList.toggle('nav-open');
-});
+function fixNavAfterUserScroll(){
+	window.addEventListener('scroll', function(e){
+		var scrollTop = window.pageYOffset;
+		var imageHeight = document.getElementById('header').clientHeight;
+		var navigation = document.getElementById('navigation');
+		var headerText = document.getElementById('header-text');
 
 
+		if(scrollTop > imageHeight){
+			navigation.classList.add('fixed-nav');
+			headerText.classList.add('fixed-logo');
+		}
+		else{
+			navigation.classList.remove('fixed-nav');
+			headerText.classList.remove('fixed-logo');
+		}
+	});
+}
 
+function toggleNavOnClick(){
+	document.getElementById('nav-icon').addEventListener('click',function(){
+		var navLinks = document.getElementById('nav-links');
+
+		navLinks.classList.toggle('nav-open');
+	});
+}
+
+function hideNavOnLinkClick(){
+	var navLinksArray = Array.from(document.querySelectorAll('.links'));
+		
+	navLinksArray.forEach(function(element){
+		element.addEventListener('click',function(){
+			var navLinks = document.getElementById('nav-links');
+			navLinks.classList.remove('nav-open');
+		});
+	});
+}
+
+toggleNavOnClick();
+hideNavOnLinkClick();
+fixNavAfterUserScroll();
 
 
 
